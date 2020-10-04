874531e28dc7732b64f73c85600eb1c9952b2946
Mitch Phillips <mitch@Mitchs-MacBook-Pro.local>
2020-10-04 14:39:52 -0400

    secret santa img and small img python

--- python_scripts/process_img.py
+++ python_scripts/process_img.py
@@ -1,17 +1,28 @@
-# Takes in a directory path filled with images. 
-# Makes a sub directory called "small" and fills it with small square versions of the imgs
-# spits out the html code to be added to the site
+# # Takes in a directory path filled with images. 
+# # Makes a sub directory called "small" and fills it with small square versions of the imgs
+# # spits out the html code to be added to the site
 
-import os
+# import os
 
-# path = input("Enter path to folder")
+# # path = input("Enter path to folder")
 
-path = '/Users/mitch/Documents/ep-lug/collaborations/Secret_Santa_2018'
+# path = '/Users/mitch/Documents/ep-lug/collaborations/Secret_Santa_2018'
 
-# stuff = os.walk(path)
+# # stuff = os.walk(path)
 
-for root, dirs, files in os.walk(path, topdown=False):
-   for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-      print(os.path.join(root, name))
+# for root, dirs, files in os.walk(path, topdown=False):
+#    for name in files:
+#       print(os.path.join(root, name))
+#    for name in dirs:
+#       print(os.path.join(root, name))
+
+# Go to the eplug flickr group and click on first photo
+# Download Large and Small versions of each photo until it lands on a url it has already downloaded
+# Keep track of images already downloaded in a csv.
+
+FIRST_PHOTO_URL = "https://www.flickr.com/photos/134183198@N05/50310500902/in/pool-14665180@N23/"
+
+# <a class="navigate-target navigate-next" href="/photos/_czq_/50284628786/in/pool-14665180@N23/" title="Heavy Duty | by _CZQ_" data-track="nextPhotoButtonClick" data-action="ad" style="height: 591px;"><span class="hide-text">→</span></a>
+
+from bs4 import BeautifulSoup
+
---
+++ python_scripts/small_img.py
@@ -0,0 +1,32 @@
+# PATH = input("Enter the path to your images")
+PATH = "../collaborations/Secret_Santa_2018/images/"
+import os 
+
+# Check to make sure the path exists and is writeable
+# if os.access(PATH, os.W_OK):
+#     print("Path is ok")
+# else:
+
+# Get list of images in DIR with os walk
+
+from PIL import Image
+
+images = []
+
+for root, dirs, files in os.walk(PATH):
+    for file in files:
+    	if file[-3:] == 'jpg' and file[-9:-4] != 'thumb':
+    		images.append(file)
+        # print(len(path) * '---', file)
+
+
+# Make a thumbnail size image with _thumb at end of name
+for image_path in images :
+	img = Image.open(PATH + image_path)
+
+	img.thumbnail((2000, 400))
+	img.save(PATH + image_path[:-4] + '_thumb.jpg')
+	print("<a href='images/" + image_path + "'><img src='images/"+image_path[:-4] + '_thumb.jpg' +"'></a>")
+
+
+# todo pass in a flickr gallery url and get all the images from it
